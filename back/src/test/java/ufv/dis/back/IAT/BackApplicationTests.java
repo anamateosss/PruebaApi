743452d1e503dbb8dc08980c648ada9d771e7d4d
Ana Mateos <9204003@alumnos.ufv.es>
2025-01-17 12:03:27 +0100

    Commit inicial

---
+++ back/src/test/java/ufv/dis/back/IAT/BackApplicationTests.java
@@ -0,0 +1,13 @@
+package ufv.dis.back.IAT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
